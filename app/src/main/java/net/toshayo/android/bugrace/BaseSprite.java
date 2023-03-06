22a950fdc038e0eb0b5d86846d7303d127d14b2e
Toshayo <abc126cba.abc126cba@yandex.ru>
2023-03-06 14:13:11 +0100

    Centered sprite

--- app/src/main/java/net/toshayo/android/bugrace/BaseSprite.java
+++ app/src/main/java/net/toshayo/android/bugrace/BaseSprite.java
@@ -15,13 +15,16 @@ public class BaseSprite implements IDrawable {
     }
 
     public void setPosition(int x, int y) {
-        this.x = x;
-        this.y = y;
+        this.x = (int)(x - width / 2F);
+        this.y = (int)(y - height / 2F);
     }
 
     public void setSize(int width, int height) {
+        this.x = (int)(this.x + this.width / 2F);
+        this.y = (int)(this.y + this.height / 2F);
         this.width = width;
         this.height = height;
+        setPosition(this.x, this.y);
     }
 
     public boolean intersectsWith(BaseSprite other) {
