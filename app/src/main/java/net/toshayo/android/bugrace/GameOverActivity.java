f6a1fca8a598553ecb5f4c657bb18fc6a39c1026
Toshayo <abc126cba.abc126cba@yandex.ru>
2023-03-13 11:22:56 +0100

    Merge left-overs

--- app/src/main/java/net/toshayo/android/bugrace/GameOverActivity.java
+++ app/src/main/java/net/toshayo/android/bugrace/GameOverActivity.java
@@ -1,13 +1,11 @@
 package net.toshayo.android.bugrace;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.io.IOException;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GameOverActivity extends AppCompatActivity {
     @Override
