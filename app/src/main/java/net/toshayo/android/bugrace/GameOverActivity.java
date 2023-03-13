e8f4cce8e350b7907ae21df87ec6b2225bece01e
Toshayo <abc126cba.abc126cba@yandex.ru>
2023-03-13 11:22:03 +0100

    Screen with start button appears on app launch

--- app/src/main/java/net/toshayo/android/bugrace/GameOverActivity.java
+++ app/src/main/java/net/toshayo/android/bugrace/GameOverActivity.java
@@ -18,7 +18,10 @@ public class GameOverActivity extends AppCompatActivity {
         MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.game_over);
         mediaPlayer.start();
 
-        ((TextView)findViewById(R.id.lblScore)).setText(getString(R.string.lblScore, getIntent().getIntExtra("score", 0)));
+        if(getIntent().hasExtra("score"))
+            ((TextView)findViewById(R.id.lblScore)).setText(getString(R.string.lblScore, getIntent().getIntExtra("score", 0)));
+        else
+            ((TextView)findViewById(R.id.lblScore)).setText("");
 
         findViewById(R.id.btnStart).setOnClickListener(v -> {
             startActivity(new Intent(this, MainActivity.class));
